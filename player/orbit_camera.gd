197513d00857ca16d1a205e0c69806759f8b5b33
jiangwanyi <jiangwane@qq.com>
2024-01-31 16:11:52 +0800

    基础的摄像头遮挡移动

--- player/orbit_camera.gd
+++ player/orbit_camera.gd
@@ -15,9 +15,12 @@ var vertical_angle = 0.0    # 垂直方向的初始角度
 
 @export var autoturn_ray_aperture := 25.0
 
+var collision_exception: Array[RID] = []
+
 func _ready():
 	set_process_input(true)
-
+	# 将摄像机从父空间节点中分离。
+	set_as_top_level(true)
 	# 隐藏鼠标并锁定其位置
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -32,25 +35,93 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta):
+	# 角色位置
 	var target := (get_parent() as Node3D).global_transform.origin
+	# 摄像机位置
 	var pos := get_global_transform().origin
 	
-	var difference := pos - target
+	var ds := PhysicsServer3D.space_get_direct_state(get_world_3d().get_space())
 	
-	var root = get_tree().get_root()
+	## 左侧、中心和右侧射线检测。
+	#var col_left = ds.intersect_ray(PhysicsRayQueryParameters3D.create(
+			#target,
+			#target + Basis(Vector3.UP, deg_to_rad(autoturn_ray_aperture)) * (difference),
+			#0xFFFFFFFF,
+			#collision_exception
+	#))
 	
-	var line = MeshInstance3D.new()
-	var mesh = ImmediateMesh.new()
-	var material = StandardMaterial3D.new()
-	material.albedo_color = Color.RED
+	# 从角色到摄像机的射线
+	var col = ds.intersect_ray(PhysicsRayQueryParameters3D.create(
+			target,
+			pos,
+			0xFFFFFFFF,
+			collision_exception
+	))
 	
-	line.mesh = mesh
-	mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-	mesh.surface_add_vertex(target)
-	mesh.surface_add_vertex(pos)
-	mesh.surface_end()
+	if not col.is_empty():
+		radius = (col.position as Vector3).distance_to(target)
+		print(radius)
+	else:
+		radius = max_radius
+		
+	#pos = target + difference
 	
-	root.add_child(line)
+	# 根据新的 radius 计算摄像机位置
+	var x = radius * cos(vertical_angle) * sin(horizontal_angle)
+	var y = radius * sin(vertical_angle)
+	var z = radius * cos(vertical_angle) * cos(horizontal_angle)
+
+	global_transform.origin = Vector3(x, y, z) + target
+	look_at(target, Vector3.UP)
+	
+	#var col_right = ds.intersect_ray(PhysicsRayQueryParameters3D.create(
+			#target,
+			#target + Basis(Vector3.UP, deg_to_rad(-autoturn_ray_aperture)) * (difference),
+			#0xFFFFFFFF,
+			#collision_exception
+	#))
+	#
+	#var root = get_tree().get_root()
+	#
+	#var line_col_left = MeshInstance3D.new()
+	#var mesh_left = ImmediateMesh.new()
+	#
+	#var line_col_right = MeshInstance3D.new()
+	#var mesh_right = ImmediateMesh.new()
+	#
+	#var line_col_center = MeshInstance3D.new()
+	#var mesh_center = ImmediateMesh.new()
+	#
+	#var material_red = StandardMaterial3D.new()
+	#material_red.albedo_color = Color.RED
+	#
+	#var material_blue = StandardMaterial3D.new()
+	#material_blue.albedo_color = Color.BLUE
+	#
+	#var material_green = StandardMaterial3D.new()
+	#material_green.albedo_color = Color.GREEN
+	#
+	#line_col_left.mesh = mesh_left
+	#mesh_left.surface_begin(Mesh.PRIMITIVE_LINES, material_red)
+	#mesh_left.surface_add_vertex(target)
+	#mesh_left.surface_add_vertex(target + Basis(Vector3.UP, deg_to_rad(autoturn_ray_aperture)) * (difference))
+	#mesh_left.surface_end()
+	#
+	#line_col_center.mesh = mesh_center
+	#mesh_center.surface_begin(Mesh.PRIMITIVE_LINES, material_green)
+	#mesh_center.surface_add_vertex(target)
+	#mesh_center.surface_add_vertex(target + difference)
+	#mesh_center.surface_end()
+	#
+	#line_col_right.mesh = mesh_right
+	#mesh_right.surface_begin(Mesh.PRIMITIVE_LINES, material_blue)
+	#mesh_right.surface_add_vertex(target)
+	#mesh_right.surface_add_vertex(target + Basis(Vector3.UP, deg_to_rad(-autoturn_ray_aperture)) * (difference))
+	#mesh_right.surface_end()
+	#
+	#root.add_child(line_col_left)
+	#root.add_child(line_col_center)
+	#root.add_child(line_col_right)
 
 	#line.mesh.surface_add_vertex(target + Basis(Vector3.UP, deg_to_rad(autoturn_ray_aperture)) * Vector3(1, 1, 1))
 	#line.mesh.surface_add_vertex(pos)
@@ -73,11 +144,3 @@ func _physics_process(delta):
 
 	# 确保 radius 在合理范围内
 	#radius = clamp(radius, min_radius, max_radius)
-
-	# 根据新的 radius 计算摄像机位置
-	var x = radius * cos(vertical_angle) * sin(horizontal_angle)
-	var y = radius * sin(vertical_angle)
-	var z = radius * cos(vertical_angle) * cos(horizontal_angle)
-
-	global_transform.origin = Vector3(x, y, z) + target
-	look_at(target, Vector3.UP)
